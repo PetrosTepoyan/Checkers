97aefb571788c18c49216c80f1ba6492d88e1f71
PetrosTepoyan <petros@tepoyan.com>
2024-02-10 15:53:29 +0400

    Initial Commit

---
+++ Checkers/CheckersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckersApp.swift
+//  Checkers
+//
+//  Created by Petros Tepoyan on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CheckersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
